75bda61956dec69d98a1506baaf73c71e141b756
Alessandro Catucci <graphic.alessandro@gmail.com>
2022-07-18 12:00:05 +0200

    start Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 //import style scss
 import './App.scss';
 //import components
+import Header from './component/Header';
 import Navbar from './component/navbar/Navbar';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Header />
     </>
   );
 }
