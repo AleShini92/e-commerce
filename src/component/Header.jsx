f341cb5b5367d7a12d07fce9fe334cd5ae3cfd33
Alessandro Catucci <graphic.alessandro@gmail.com>
2022-07-18 18:10:05 +0200

    added folder services and products file

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 
+import headerBg from '../assets/headerBg.png';
+
 const Header = () => {
     return (
         <>
-        
+            <header>
+                <div className="wrapper-header bg-dark-blue p-1">
+                    <figure>
+                        <figcaption>
+                            <h1>New seasons arrivals</h1>
+                            <p>check out all the trands</p>
+                        </figcaption>
+                        <img src={headerBg} alt="woman dresses the blue dress" title="image background"/>
+                    </figure>
+                </div>
+            </header>
         </>
     );
 }
