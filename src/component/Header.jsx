ba0bf4afcb5bb7a5ecdb46fdbf6c2c3567b6de8c
Shini <graphic.alessandro@gmail.com>
2023-01-01 11:55:16 +0100

    added image to home

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 
-// import headerBg from '../assets/headerBg.png';
+import headerBg from '../assets/headerBg.png';
 
 const Header = () => {
     return (
@@ -18,7 +18,7 @@ const Header = () => {
                             <h1>New seasons arrivals</h1>
                             <p>check out all the trands</p>
                         </figcaption>
-                        {/* <img src={headerBg} alt="woman dresses the blue dress" title="image background"/> */}
+                        <img src={headerBg} alt="woman dresses the blue dress" title="image background"/>
                     </figure>
                 </div>
             </header>
